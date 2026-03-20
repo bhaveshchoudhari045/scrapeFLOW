52adf065e77c8adf1bc44115a5ccffac6073446f
Bhavesh Choudhari <bhaveshchoudhari045@users.noreply.github.com>
2026-03-21 00:28:54 +0530

    feat: improved workflow execution UI

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -11,11 +11,13 @@ export async function LaunchBrowserExecutor(
     const browser = await puppeteer.launch({
       headless: true, // for testing
     });
-
+    environment.log.info("Browser started successfully");
     environment.setBrowser(browser);
     const page = await browser.newPage();
     await page.goto(websiteUrl);
     environment.setPage(page);
+    environment.log.info(`Opened  page at: ${websiteUrl}`);
+
     return true;
   } catch (error: any) {
     environment.log.error(error.message);
--- lib/workflow/executor/PageToHtmlExecutor.ts
+++ lib/workflow/executor/PageToHtmlExecutor.ts
@@ -7,13 +7,14 @@ export async function PageToHtmlExecutor(
   environment: ExecutionEnvironment<typeof PageToHtmlTask>,
 ): Promise<boolean> {
   try {
+    throw new Error("test");
     const html = await environment.getPage()!.content();
     console.log("@PAGE HTML", html);
     environment.setOutput("Html", html);
 
     return true;
-  } catch (error) {
-    console.error(error);
+  } catch (error: any) {
+    environment.log.error(error.message);
     return false;
   }
 }
